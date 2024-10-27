dd107e64515ce5e7bb99cd86999914e5dbfe2747
Ammon-Faust <ammonfaust@gmail.com>
2024-10-26 21:26:32 -0600

    update: typo, comments in test_htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,41 +3,49 @@ import unittest
 from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
+    # Test that HTMLNode can be initialized with None values
     def test_None(self):
         node = HTMLNode(None, None, None, None)
     
+    # Test that multiple properties are correctly converted to HTML
     def test_props_to_html_with_multiple_props(self):
         node = HTMLNode(props={"href": "https://www.google.com", "target": "_blank"})
         expected_output = ' href="https://www.google.com" target="_blank"'  # Ensure there is a space here
         self.assertEqual(node.props_to_html(), expected_output)
 
+    # Test when no props are passed
     def test_props_to_html_with_no_props(self):
         node = HTMLNode()
         expected_output = ""
         self.assertEqual(node.props_to_html(), expected_output)
 
+    # Test single prop being passed
     def test_props_to_html_with_single_prop(self):
         props = {"class": "test-class"}
         node = HTMLNode(props=props)
         expected_output = ' class="test-class"'
         self.assertEqual(node.props_to_html(), expected_output)
 
-class testLeafNode(unittest.TestCase):
+class TestLeafNode(unittest.TestCase):
+    # Test that a value is required
     def test_empty_value(self):
         node = LeafNode(value=None, tag=None, props=None)
         with self.assertRaises(ValueError):
             node.to_html()
 
+    # Test if an empty tag return raw value
     def test_empty_tag(self):
         node = LeafNode("This is a test!", None, None)
         expected_output = "This is a test!"
         self.assertEqual(node.to_html(), expected_output)
     
+    # Test that tags are properly appended
     def test_tag(self):
         node = LeafNode("This is a test!", "p", None)
         expected_output = "<p>This is a test!</p>"
         self.assertEqual(node.to_html(), expected_output)
 
+    # Test if props append correctly
     def test_props(self):
         node = LeafNode("This is a test!", "a", props={"href": "https://www.google.com", "target": "_blank"})
         expected_output = '<a href="https://www.google.com" target="_blank">This is a test!</a>'
