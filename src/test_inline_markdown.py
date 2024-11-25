f76fe166e68aade75847678bd75c4d4e05ed8dad
Ammon-Faust <ammonfaust@gmail.com>
2024-11-24 20:08:24 -0700

    organize: created inline file to cleanup code/tests

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,125 @@
+import unittest
+
+from inline_markdown import *
+from textnode import *
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_no_text_type(self):
+        node = TextNode("**bold**", TextType.BOLD)
+        nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(1, len(nodes))
+        self.assertEqual("**bold**", nodes[0].text)
+        self.assertEqual(TextType.BOLD, nodes[0].text_type)
+
+    def test_missing_delimiter(self):
+        node = TextNode("Hello **bold", TextType.TEXT)
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "**", TextType.BOLD)
+
+    def test_simple_split(self):
+        node = TextNode("This **is** a test!", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(3, len(nodes))
+        self.assertEqual("This ", nodes[0].text)
+        self.assertEqual(TextType.TEXT, nodes[0].text_type)
+        self.assertEqual("is", nodes[1].text)
+        self.assertEqual(TextType.BOLD, nodes[1].text_type)
+        self.assertEqual(" a test!", nodes[2].text)
+        self.assertEqual(TextType.TEXT, nodes[2].text_type)
+
+    def test_multiple_delimiters(self):
+        node = TextNode("Hello **bold** world **again**", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        self.assertEqual(5, len(nodes))
+        self.assertEqual("Hello ", nodes[0].text)
+        self.assertEqual(TextType.TEXT, nodes[0].text_type)
+        self.assertEqual("bold", nodes[1].text)
+        self.assertEqual(TextType.BOLD, nodes[1].text_type)
+        self.assertEqual(" world ", nodes[2].text)
+        self.assertEqual(TextType.TEXT, nodes[2].text_type)
+        self.assertEqual("again", nodes[3].text)
+        self.assertEqual(TextType.BOLD, nodes[3].text_type)
+        self.assertEqual("", nodes[4].text)
+        self.assertEqual(TextType.TEXT, nodes[4].text_type)
+
+
+class TestMarkdownImages(unittest.TestCase):
+    def test_single_image(self):
+        node = extract_markdown_images("![rick roll](https://i.imgur.com/aKaOqIh.gif)")
+        expected_output = [("rick roll", "https://i.imgur.com/aKaOqIh.gif")]
+        self.assertEqual(node, expected_output)
+
+    def test_multiple_images(self):
+        node = extract_markdown_images(
+            """This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) 
+            and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"""
+        )
+        expected_output = [
+            ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+            ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
+        self.assertEqual(node, expected_output)
+
+    def test_no_alt_text_images(self):
+        with self.assertRaises(ValueError):
+            node = extract_markdown_images("![](https://i.imgur.com/aKaOqIh.gif)")
+
+
+class TestMarkdownLinks(unittest.TestCase):
+    def test_single_link(self):
+        node = extract_markdown_links("[to boot dev](https://www.boot.dev)")
+        expected_output = [("to boot dev", "https://www.boot.dev")]
+        self.assertEqual(node, expected_output)
+
+    def test_multiple_links(self):
+        node = extract_markdown_links(
+            """This is text with a link [to boot dev](https://www.boot.dev)
+            and [to youtube](https://www.youtube.com/@bootdotdev)"""
+        )
+        expected_output = [
+            ("to boot dev", "https://www.boot.dev"),
+            ("to youtube", "https://www.youtube.com/@bootdotdev"),
+        ]
+        self.assertEqual(node, expected_output)
+
+    def test_no_alt_text_links(self):
+        with self.assertRaises(ValueError):
+            node = extract_markdown_links("[](https://www.boot.dev)")
+
+
+class TestSplitImagesAndLinks(unittest.TestCase):
+    def test_split_nodes_image(self):
+        node = TextNode("Just plain text", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        assert len(nodes) == 1
+        assert nodes[0].text == "Just plain text"
+
+        # Test 2: One image
+        node = TextNode("Hello ![test](test.png) world", TextType.TEXT)
+        nodes = split_nodes_image([node])
+        assert len(nodes) == 3
+        assert nodes[0].text == "Hello "
+        assert nodes[1].text == "test"
+        assert nodes[1].text_type == TextType.IMAGE
+        assert nodes[1].url == "test.png"
+
+    def test_split_nodes_link(self):
+        # Test 1: No links
+        node = TextNode("Just plain text", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        assert len(nodes) == 1
+        assert nodes[0].text == "Just plain text"
+
+        # Test 2: One link
+        node = TextNode("Hello [test](https://google.com) world", TextType.TEXT)
+        nodes = split_nodes_link([node])
+        assert len(nodes) == 3
+        assert nodes[0].text == "Hello "
+        assert nodes[1].text == "test"
+        assert nodes[1].text_type == TextType.LINK
+        assert nodes[1].url == "https://google.com"
+
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,8 +1,6 @@
 import re
 from enum import Enum
-from warnings import warn
-
-from htmlnode import *
+from htmlnode import LeafNode
 
 
 class TextType(Enum):  # Enum of text types we will take
@@ -47,92 +45,3 @@ def text_node_to_html_node(text_node):  # Takes text node and turns it into a Le
     raise ValueError("Invalid TextType")
 
 
-def split_nodes_delimiter(
-    old_nodes, delimiter, text_type
-):  # Splits a list of nodes by a delimiter
-    new_nodes = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-        else:
-            parts = node.text.split(delimiter)
-            if len(parts) % 2 == 0:
-                raise ValueError("Missing closing delimiter")
-            for i, _ in enumerate(parts):
-                if i % 2 == 0:
-                    current_type = TextType.TEXT
-                    new_nodes.append(TextNode(parts[i], current_type))
-                else:
-                    current_type = text_type
-                    new_nodes.append(TextNode(parts[i], current_type))
-    return new_nodes
-
-
-def extract_markdown_images(text):
-    image_texts = re.findall(r"!\[([^\[\]]*?)\]\(([^\(\)]*?)\)", text)
-    for alt_text, _ in image_texts:
-        if alt_text == "":
-            raise ValueError("Missing alt text")
-    return image_texts
-
-
-def extract_markdown_links(text):
-    link_texts = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-    for alt_text, _ in link_texts:
-        if alt_text == "":
-            raise ValueError("Missing alt text")
-    return link_texts
-
-
-def split_nodes_image(old_nodes):
-    new_nodes = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-            continue
-
-        images = extract_markdown_images(node.text)
-        if not images:
-            new_nodes.append(node)
-            continue
-
-        current_text = node.text
-        for alt_text, url in images:
-            parts = current_text.split(f"![{alt_text}]({url})", 1)
-            if parts[0]:
-                new_nodes.append(TextNode(parts[0], TextType.TEXT))
-
-            new_nodes.append(TextNode(alt_text, TextType.IMAGE, url))
-            current_text = parts[1]
-
-        if current_text:
-            new_nodes.append(TextNode(current_text, TextType.TEXT))
-
-    return new_nodes
-
-
-def split_nodes_link(old_nodes):
-    new_nodes = []
-    for node in old_nodes:
-        if node.text_type != TextType.TEXT:
-            new_nodes.append(node)
-            continue
-
-        links = extract_markdown_links(node.text)
-        if not links:
-            new_nodes.append(node)
-            continue
-
-        current_text = node.text
-        for alt_text, url in links:
-            parts = current_text.split(f"[{alt_text}]({url})", 1)
-            if parts[0]:
-                new_nodes.append(TextNode(parts[0], TextType.TEXT))
-
-            new_nodes.append(TextNode(alt_text, TextType.LINK, url))
-            current_text = parts[1]
-
-        if current_text:
-            new_nodes.append(TextNode(current_text, TextType.TEXT))
-
-    return new_nodes
